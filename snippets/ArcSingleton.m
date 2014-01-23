a1159ce3134a437bc7bb4771a69b6c63bae67864
Viktor Malieichyk <mwchief@gmail.com>
2014-01-23 15:28:05 +0200

    Added arc singleton template. Minor fixes.

---
+++ snippets/ArcSingleton.m
@@ -0,0 +1,38 @@
+#import "ArcSingleton.h"
+
+static ArcSingleton *sharedInstance = nil;
+
+@implementation ArcSingleton
+
+@synthesize someProperty;
+
+#pragma mark Singleton Methods
+
++ (instancetype)sharedInstance {
+  static dispatch_once_t pred = 0;
+  __strong static id _sharedObject = nil;
+  dispatch_once(&pred, ^{
+    _sharedObject = [[self alloc] init]; // or some other init method
+  });
+  return _sharedObject;
+}
+
+
+- (instancetype)copyWithZone:(NSZone *)zone {
+  return self;
+}
+
+- (instancetype)init {
+  if (self = [super init]) {
+      someProperty = [[NSString alloc] initWithString:@"Default Property Value"];
+  }
+  return self;
+}
+
+
+- (void)dealloc {
+  // Should never be called, but just here for clarity really.
+  [super dealloc];
+}
+
+@end
---
+++ snippets/NonArcSingleton.m
@@ -0,0 +1,60 @@
+#import "NonArcSingleton.h"
+
+static NonArcSingleton *sharedInstance = nil;
+
+@implementation NonArcSingleton
+
+@synthesize someProperty;
+
+#pragma mark Singleton Methods
+
++ (instancetype)sharedInstance {
+  static dispatch_once_t pred = 0;
+  __strong static id _sharedObject = nil;
+  dispatch_once(&pred, ^{
+    _sharedObject = [[self alloc] init]; // or some other init method
+  });
+  return _sharedObject;
+}
+
+
+- (instancetype)copyWithZone:(NSZone *)zone {
+  return self;
+}
+
+
+- (instancetype)retain {
+  return self;
+}
+
+
+- (NSUInteger)retainCount {
+  return UINT_MAX; //denotes an object that cannot be released
+}
+
+
+- (oneway void)release {
+  // never release
+}
+
+
+- (instancetype)autorelease {
+  return self;
+}
+
+
+- (instancetype)init {
+  if (self = [super init]) {
+      someProperty = [[NSString alloc] initWithString:@"Default Property Value"];
+  }
+  return self;
+}
+
+
+- (void)dealloc {
+  // Should never be called, but just here for clarity really.
+  [someProperty release];
+  [super dealloc];
+}
+
+@end
